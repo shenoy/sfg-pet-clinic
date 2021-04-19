0069366d61c9b67c8f11657386ec1187f36ea1bf
shenoy <rajeshtshenoy@gmail.com>
2021-04-19 21:32:06 +0100

    Adding Bootstrap for data. Closes #14.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
@@ -1,11 +1,11 @@
 package guru.springframework.sfgpetclinic.services.map;
 
 import guru.springframework.sfgpetclinic.model.Pet;
-import guru.springframework.sfgpetclinic.services.CrudService;
+import guru.springframework.sfgpetclinic.services.PetService;
 
 import java.util.Set;
 
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
         return super.findAll();
@@ -14,13 +14,11 @@ public class PetServiceMap extends AbstractMapService<Pet, Long> implements Crud
     @Override
     public void deleteByID(Long id) {
         super.deleteByID(id);
-
     }
 
     @Override
     public void delete(Pet object) {
         super.delete(object);
-
     }
 
     @Override
