f0e5cc5e9934371d5f48e59199247fd4c752f01e
shenoy <rajeshtshenoy@gmail.com>
2021-04-25 13:35:00 +0100

    Add missing i18n files . Closes #25

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-
     @GetMapping({"/","", "index", "index.html"})
     public String index(){
         return "index";
