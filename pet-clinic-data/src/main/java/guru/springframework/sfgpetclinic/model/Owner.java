ba375bacc2805af6ff6320fbd5fff579bf05ee04
shenoy <rajeshtshenoy@gmail.com>
2021-04-25 20:18:09 +0100

    Added Owner Contact Info. Closes #28.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -3,7 +3,25 @@ package guru.springframework.sfgpetclinic.model;
 import java.util.Set;
 
 public class Owner extends Person{
+    private String address;
+    private String telephone;
     private Set<Pet> pets;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 
     public Set<Pet> getPets() {
         return pets;
